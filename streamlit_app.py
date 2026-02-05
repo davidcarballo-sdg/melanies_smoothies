c9fb3a85b395bb97fb2b2ac59e6c40c01eac9fa1
davidcarballo-sdg <david.carballo@sdggroup.com>
2026-02-05 20:04:25 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,6 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 # Convert snowpark dataframe to pandas
 pd_df=my_dataframe.to_pandas()
 st.dataframe(pd_df)
-st.stop()
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredientes: '
